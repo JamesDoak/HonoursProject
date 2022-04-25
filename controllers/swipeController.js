4bb64974690427a6856bc211d5c9bc97443bcb38
jdoak200@caledonian.ac.uk <jdoak200@caledonian.ac.uk>
2022-04-25 16:18:08 +0100

    Access checks implemented - no access = no swipe

--- controllers/swipeController.js
+++ controllers/swipeController.js
@@ -184,9 +184,23 @@ exports.show_add_employee_swipe = function (req, res) {
 
 
 exports.add_swipe = function(req, res){
+    var isAllowed = Boolean(req.body.eAllowed);
     var user = req.user.user;
-    db.add_user_swipe(req.params.id, req.body.empName, req.body.eID, user);
-    res.redirect('/');
+
+    if(isAllowed == true){
+        db.add_user_swipe(req.params.id, req.body.empName, req.body.eID, user);
+        res.redirect('/');
+    }
+    else if(isAllowed == false){
+        res.render('500', {
+            'title': 'Revoked',
+            'empError':'Permission to swipe DENIED - Employee privileges have been revoked.',
+            'user' : req.user.user
+        });
+    }
+
+
+
 }
 
 
@@ -211,66 +225,3 @@ exports.edit_employee = function(req, res){
     edb.edit_employee(req.params.id, req.body.empName, req.body.eID, req.body.ePos, Boolean(req.body.eAllowed), user);
     res.redirect('/all_employees');
 }
-
-
-///////////////////////////////////////////////////////
-// ACCOUNT RELATED //
-///////////////////////////////////////////////////////
-
-// exports.logout = function(req, res){
-//     req.logout();
-//     res.redirect('/');
-// }
-
-
-// exports.show_login_page = function(req, res){
-//     res.render('login', {
-//         'title': 'Login',
-//         'background':'#E6E6FA'
-//     });
-// }
-
-// exports.post_login = function(req, res){
-//     console.log('serialisedUser wrote', req.session.passport.user);
-//     res.redirect('/');
-// }
-
-// exports.show_register_page = function(req, res){
-//     res.render('register', {
-//         'title' : 'Registration',
-//         'background' : '#E6E6FA'
-//     });
-// }
-
-// exports.account = function(req, res){
-//     res.render('account', {
-//         'title': 'My Account',
-//         'background':'#E6E6FA',
-//         'user' : req.user.user
-//     });
-// }
-
-
-// exports.post_new_user = function(req, res){
-//     // const name = req.body.fname;
-//     const user = req.body.username;
-//     const password = req.body.pass;
-//     console.log("register user: ", user, " / password: ", password);
-
-//     if(!user || !password){
-//         res.send(401, 'No entry for: Full Name, or Email, or Password');
-//         return;
-//     }
-//     uClass.lookup(user, function(err, u) {
-//         if(u) {
-//             res.render('500', {
-//                 'title': '500 Error',
-//                 'background':'#E6E6FA',
-//                 'err' : "User exists, please use another email address. If you own this email address and didn't make this account, email us on admin@admin.com"
-//             });
-//             return;
-//         }
-//         uClass.create(user, password);
-//         res.redirect('/login');
-//     })
-// }
